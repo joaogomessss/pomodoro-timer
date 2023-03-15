43eb4f00527dc34d78fe58470bdd6934a2b2fb92
João Gomes <joaossemog@gmail.com>
2023-03-15 19:16:38 -0300

    x

--- javascript.js
+++ javascript.js
@@ -1,14 +1,21 @@
 
-let requestId;
 
 
-let startButton = document.querySelector("#start-button").onclick  = () => start(); ;
+
+
+let startButton = document.querySelector("#start-button");
+startButton.onclick  = () => start();
+
+
 let editButton = document.querySelector("#edit-button").onclick = () => Edit();
 
 let display = document.querySelector("#display")
 
 let loop;
-let remainingTime;
+let remainingTime
+let initialTime;
+let interTime;
+
 
 let hours;
 let minutes;
@@ -18,15 +25,14 @@ let taskChoosen;
 let ac = { h:"" , m:"" , s:""  };
 
 function start() {   
-
-   
-let initialTime = new  Date().getTime()+ 1000 + hours * 3600000 + minutes * 60000 + seconds * 1000; 
+    
+ initialTime = new  Date().getTime() + 1000 + hours * 3600000 + minutes * 60000 + seconds * 1000; 
  
 
 loop = setInterval(function() {
 
 
-let interTime = new  Date().getTime() ;
+interTime = new  Date().getTime() ;
 
 remainingTime = Math.floor((initialTime - interTime) / 1000)  ;
 
@@ -40,7 +46,7 @@ if(seconds < 10 ){ seconds = "0" + seconds };
 
 display.textContent = hours + ":" + minutes + ":" + seconds ;
 
-console.log(remainingTime);
+
 
 
 
@@ -48,37 +54,41 @@ console.log(remainingTime);
 if(remainingTime == 0 ){ 
     
     clearInterval(loop)  ; 
-
+    loop = null;
     let newTab = window.open();
     newTab.alert(taskChoosen);
 
+
+    hours = ac.h;
+    minutes = ac.m;
+    seconds = ac.s;
+    taskChoosen = taskChoosen;
 }
 
+update(remainingTime);
 
-},1000);
+},1000)
 
 
-
-
+startButton.textContent = "pause";
+startButton.onclick = () => pause();
 };
 
 
 
 
+function update(remainingTime){
 
-function checkVisibility() {
-
-if(document.hidden){
-
-cancelAnimationFrame(requestId);
-
-}else { requestId = requestAnimationFrame(checkVisibility)          }
 
 };
 
 
+function pause(){
+
+ if(startButton.textContent == "pause" ){  clearInterval(loop) ; startButton.textContent = "Start";   }  else {   start() ;   } 
 
 
+};
 
 function Edit() {
 
@@ -93,19 +103,26 @@ body.append(hou,min,sec,task,subButton);
 
 subButton.onclick = function(){       
 
+ ac.h = hou.value;
+ac.m = min.value;
+ac.s = sec.value;
+taskChoosen = task.value;
+
+
 hours = hou.value;
 minutes = min.value;
 seconds = sec.value;
 taskChoosen = task.value;
 
-ac.h = hou.value;
-ac.m = min.value;
-ac.s = sec.value;
-taskChoosen = task.value;
+if(hou.value  == "" ){ hours = 0 };
+if(min.value  == "" ){ minutes = 0 };
+if(sec.value  == "" ){ seconds =  0 };
 
-if(hours   < 10 ){ hours   = "0" + hours };
-if(minutes < 10 ){ minutes = "0" + minutes };
-if(seconds < 10 ){ seconds = "0" + seconds };
+if(hou.value  < 10 ){ hours   = "0" + hours };
+if(min.value < 10 ){ minutes = "0" + minutes };
+if(sec.value < 10 ){ seconds = "0" + seconds };
+
+
 
 display.textContent = hours + ":" + minutes + ":" + seconds ;
 
@@ -119,4 +136,8 @@ subButton.remove();
 
 }
 
-checkVisibility();
+
+
+
+
+
